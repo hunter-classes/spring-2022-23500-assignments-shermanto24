895b7d37cbd1b4a568c27595c4c84492a6d92f74
stef <sc.shher@gmail.com>
2022-02-06 13:57:44 -0500

    Finished memoization version of stepPerms

--- assignments/recursion/staircase.cpp
+++ assignments/recursion/staircase.cpp
@@ -11,11 +11,38 @@ int stepPerms(int n) //n = the number of steps
     return stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
 }
 
+int steptable[10000];
+
+int stepPerms_dyn(int steps)
+{
+  if (steps == 0)
+    return 1;
+  else if (steptable[steps] != 0)
+    return steptable[steps];
+  else
+  {
+    int w = 0; //w = # of ways we can do steps-1, steps-2, steps-3 
+    for (int i = 3; i >= 1; i--)
+    {
+      int r = steps - i; //r = steps remaining
+      if (r >= 0)
+      {
+        int temp = w + stepPerms_dyn(r);
+        w = temp;
+        steptable[steps] = temp;
+      }
+    }
+    return w;
+  }
+}
+
 int main()
 {
-  std::cout << "stepPerms(5) = " << stepPerms(5) << '\n';
-  std::cout << "stepPerms(3) = " << stepPerms(3) << '\n';
-  std::cout << "stepPerms(1) = " << stepPerms(1) << '\n';
-  std::cout << "stepPerms(10) = " << stepPerms(10) << '\n';
-  std::cout << "stepPerms(12) = " << stepPerms(12) << '\n';
+  for (int i = 0; i <= 10; i++)
+    std::cout << i << ": " << stepPerms(i) << '\n';
+
+  std::cout << '\n';
+
+  for (int i = 0; i <= 10; i++)
+    std::cout << i << ": " << stepPerms_dyn(i) << '\n';
 }
