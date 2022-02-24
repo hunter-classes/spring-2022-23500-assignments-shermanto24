eef304497a6aa761b6e1901442fc0c76b78bc660
stef <sc.shher@gmail.com>
2022-02-24 18:04:28 -0500

    Added same changes from other folder

--- assignments/recursive-search/main.cpp
+++ assignments/recursive-search/main.cpp
@@ -6,6 +6,7 @@ int main()
   int** board = new int*[5];
   initialize_board(board);
   //print_board(board);
+  std::cout << "Solution with bottom left corner as start\n" << '\n';
   solve(board, 4, 0, 1);
   print_board(board);
   free_memory(board);
