f42c116cccb58fd119d40810b6c8b75594518c3b
stef <sc.shher@gmail.com>
2022-03-08 16:39:39 -0500

    Finished msort and added tests

--- assignments/sorts/sorts.cpp
+++ assignments/sorts/sorts.cpp
@@ -31,10 +31,10 @@ std::vector<int> ssort(std::vector<int> a)
   int i, min_index, j, tmp;
   int len = a.size();
   // loop through the vector from 0 to end
-  for (i=0;i<len; i++)
+  for (i = 0; i < len; i++)
   {
     // find the smallest value from i to the end
-    min_index = find_min_index(a,i,len);
+    min_index = find_min_index(a, i, len);
 
     // swap it into position i
     tmp = a[i];
@@ -90,16 +90,46 @@ std::vector<int> merge(std::vector<int> left, std::vector<int> right)
   return merged;
 }
 
+//---------------- my code ----------------
+std::vector<int> msort(std::vector<int> v)
+{
+  std::vector<int> result;
+
+  if (v.size() < 2)
+    return v;
+  else
+  {
+    std::vector<int> left;
+    std::vector<int> right;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+      if (i < v.size()/2)
+        left.push_back(v[i]);
+      else
+        right.push_back(v[i]);
+    }
+
+    left = msort(left);
+    right = msort(right);
+
+    result = merge(left, right);
+    return result;
+  }
+}
+
 int main()
 {
+  /*
   int size=20;
   int max_val=100;
 
   srand(time(nullptr));
   std::vector<int> a(size);
   int i;
-  for (i=0;i<size; i++)
+  for (i = 0; i < size; i++)
     a[i] = rand()%max_val;
+  */
 
   /*
     print_vector(a);
@@ -107,12 +137,35 @@ int main()
   a = ssort(a);
   print_vector(a);
   */
-  
-  std::vector<int> left = {1,2,5,6,10,15};
-  std::vector<int> right = {3,7,8,12,16,19,20};
+
+  /*
+  std::vector<int> left = {1, 2, 5, 6, 10, 15};
+  std::vector<int> right = {3, 7, 8, 12, 16, 19, 20};
   print_vector(left);
   print_vector(right);
   std::vector<int> m = merge(left,right);
   print_vector(m);
   return 0;
+  */
+
+  //my code
+  std::cout << "\nTesting msort\n" << '\n';
+
+  //testing with an even size
+  std::cout << "With an even size:" << '\n';
+  std::vector<int> v1 = {24, 5, 2, 31};
+  std::cout << "Before msort, v1 = ";
+  print_vector(v1);
+  v1 = msort(v1);
+  std::cout << "After msort, v1 = ";
+  print_vector(v1);
+
+  //testing with an odd size
+  std::cout << "\nWith an odd size:" << '\n';
+  std::vector<int> v2 = {10, 5, 2, 4, 3};
+  std::cout << "Before msort, v2 = ";
+  print_vector(v2);
+  v2 = msort(v2);
+  std::cout << "After msort, v2 = ";
+  print_vector(v2);
 }
