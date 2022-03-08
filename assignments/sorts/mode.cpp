6d23d274a51afd9069f8e5ed264c8cd69472f092
stef <sc.shher@gmail.com>
2022-03-08 16:56:10 -0500

    Implemented largest and added tests

--- assignments/sorts/mode.cpp
+++ assignments/sorts/mode.cpp
@@ -21,6 +21,19 @@ int count(std::vector<int> v, int value)
   return total;
 }
 
+//returns the largest value in vector v
+int largest(std::vector<int> v)
+{
+  if (v.empty())
+    return 0;
+
+  int max = v[0];
+  for (int i = 1; i < v.size(); i++)
+    if (v[i] > max)
+      max = v[i];
+  return max;
+}
+
 int main()
 {
   std::cout << "Testing count\n" << '\n';
@@ -30,4 +43,13 @@ int main()
   std::cout << "count(v1, 2) = " << count(v1, 2) << '\n';
   std::cout << "count(v1, 0) = " << count(v1, 0) << '\n';
   std::cout << "count(v1, 100) = " << count(v1, 100) << '\n';
+
+  std::cout << "\nTesting largest\n" << '\n';
+  std::cout << "largest(v1) = " << largest(v1) << '\n';
+  std::vector<int> v2;
+  std::cout << "v2 is an empty vector" << '\n';
+  std::cout << "largest(v2) = " << largest(v2) << '\n';
+  v2.push_back(1);
+  std::cout << "After adding the element 1 to v2, v2 = {1}" << '\n';
+  std::cout << "largest(v2) = " << largest(v2) << '\n';
 }
