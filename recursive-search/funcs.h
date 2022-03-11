196c4de34d3d2b7056ec94b858d41819bbcc61e9
stef <sc.shher@gmail.com>
2022-03-10 22:42:30 -0500

    Deleted folders

--- recursive-search/funcs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void initialize_board(int** board);
-void print_board(int** board);
-void free_memory(int** board);
-void solve(int** board, int row, int col, int counter);
-bool is_solved(int** board);
