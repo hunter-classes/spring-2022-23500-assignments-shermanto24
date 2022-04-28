692f7cc563088eeb35a5aac778d5810d62bb9cf7
stef <sc.shher@gmail.com>
2022-04-28 00:03:05 -0400

    Finished queue tests

--- assignments/stacks_queues/tests.cpp
+++ assignments/stacks_queues/tests.cpp
@@ -1,13 +1,18 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "Stack.h"
+#include "Queue.h"
+
+// ------------ testing stack ------------
 
 Stack *s = new Stack();
 
-TEST_CASE("Testing push and top")
+TEST_CASE("push, top, and is_empty")
 {
+  CHECK(s->is_empty());
   s->push("e");
   CHECK(s->top() == "e");
+  CHECK(!s->is_empty());
   s->push("d");
   CHECK(s->top() == "d");
   s->push("c");
@@ -18,7 +23,7 @@ TEST_CASE("Testing push and top")
   CHECK(s->top() == "a");
 }
 
-TEST_CASE("Testing pop: good cases")
+TEST_CASE("pop: good cases")
 {
   s->pop();
   CHECK(s->top() == "b");
@@ -31,7 +36,7 @@ TEST_CASE("Testing pop: good cases")
   s->pop();
 }
 
-TEST_CASE("Testing top and pop exceptions")
+TEST_CASE("top and pop exceptions")
 {
   try
   {
@@ -52,4 +57,75 @@ TEST_CASE("Testing top and pop exceptions")
   }
 
   delete s;
+}
+
+// ------------ testing queue ------------
+
+Queue *q = new Queue();
+
+TEST_CASE("enqueue (adding elements until full, uninterrupted), front, and is_empty")
+{
+  CHECK(q->is_empty());
+  q->enqueue(1);
+  CHECK(q->front() == 1);
+  CHECK(!q->is_empty());
+
+  q->enqueue(2);
+  CHECK(q->front() == 1);
+  q->enqueue(3);
+  q->enqueue(4);
+  q->enqueue(5);
+  CHECK(q->front() == 1);
+}
+
+TEST_CASE("enqueue: exception (full)")
+{
+  try
+  {
+    q->enqueue(6);
+  }
+  catch (int e)
+  {
+    CHECK(e == 2); //QUEUE_ERR_FULL = 2
+  }
+}
+
+TEST_CASE("dequeue: good cases, front, and enqueue (after dequeueing)")
+{
+  CHECK(q->dequeue() == 1);
+  q->enqueue(6);
+  CHECK(q->front() == 2);
+  CHECK(q->dequeue() == 2);
+  CHECK(q->dequeue() == 3);
+  q->enqueue(7);
+  CHECK(q->front() == 4);
+  CHECK(q->dequeue() == 4);
+  CHECK(q->dequeue() == 5);
+  CHECK(q->dequeue() == 6);
+  q->enqueue(8);
+  CHECK(q->dequeue() == 7);
+  CHECK(q->dequeue() == 8);
+}
+
+TEST_CASE("dequeue and front: exceptions (empty)")
+{
+  try
+  {
+    q->dequeue();
+  }
+  catch (int e)
+  {
+    CHECK(e == 1); //QUEUE_ERR_EMPTY = 1
+  }
+
+  try
+  {
+    q->front();
+  }
+  catch (int e)
+  {
+    CHECK(e == 1); //QUEUE_ERR_EMPTY = 1
+  }
+
+  delete q;
 }
