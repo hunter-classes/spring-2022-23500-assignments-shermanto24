1a5a0a808197a8fd568854b92381020b30a069e9
stef <sc.shher@gmail.com>
2022-05-01 13:15:58 -0400

    Finished rsearch tests

--- assignments/trees/tests.cpp
+++ assignments/trees/tests.cpp
@@ -2,7 +2,60 @@
 #include "doctest.h"
 #include "BSTree.h"
 
-TEST_CASE("")
+BSTree *t = new BSTree();
+
+TEST_CASE("rsearch: value exists")
 {
-  
+  t->setup();
+  t->insert(6);
+  t->insert(24);
+
+  CHECK(t->rsearch(10) == 10);
+  CHECK(t->rsearch(5) == 5);
+  CHECK(t->rsearch(3) == 3);
+  CHECK(t->rsearch(8) == 8);
+  CHECK(t->rsearch(6) == 6);
+  CHECK(t->rsearch(20) == 20);
+  CHECK(t->rsearch(15) == 15);
+  CHECK(t->rsearch(24) == 24);
+  CHECK(t->rsearch(30) == 30);
+}
+
+TEST_CASE("rsearch: value does not exist")
+{
+  try
+  {
+    t->rsearch(1);
+  }
+  catch (int e)
+  {
+    CHECK(e == 1);
+  }
+
+  try
+  {
+    t->rsearch(4);
+  }
+  catch (int e)
+  {
+    CHECK(e == 1);
+  }
+
+  try
+  {
+    t->rsearch(100);
+  }
+  catch (int e)
+  {
+    CHECK(e == 1);
+  }
+
+  try
+  {
+    t->rsearch(50);
+  }
+  catch (int e)
+  {
+    CHECK(e == 1);
+  }
 }
